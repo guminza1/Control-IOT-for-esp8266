48e1b69f24c0d0d7e6df8e705d6a8114e507c001
guminza1 <gomin_artist@hotmail.com>
2018-05-22 16:25:02 +0700

    gomin

--- 180521-210241-nodemcuv2/src/IOT_pao.ino
+++ 180521-210241-nodemcuv2/src/IOT_pao.ino
@@ -3,6 +3,7 @@
 #include <FirebaseArduino.h>
 #include <ESP8266HTTPClient.h>
 
+
 // Config connect WiFi
 #define WIFI_SSID "CG Academy Club"
 #define WIFI_PASSWORD "cg24992399"
@@ -10,7 +11,13 @@
 #define FIREBASE_HOST "iot-control-switch.firebaseio.com"
 #define FIREBASE_AUTH "Zk7iERYR8iC1kEAysVYzM6lDUeFFfCmyRimmZcYm"
 
+//varaible
+String num_day;
+int led=D4;
+
 void setup() {
+  //led status WiFi
+  pinMode(led,OUTPUT);
   // put your setup code here, to run once:
   Serial.begin(9600);
   // connect to wifi.
@@ -18,7 +25,7 @@ void setup() {
   Serial.print("connecting");
 
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+    Serial.println("reconected setup");
     delay(500);
   }
   Serial.println();
@@ -28,13 +35,24 @@ void setup() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
 
+
 }
 
 void loop() {
-  // String a = Firebase.getString("a/");
-  // Serial.println(a);
-  for (size_t i = 0; i < 1000; i++) {
-  Serial.println(i);
-  delay(10);
-}
+  // String x = Day();
+  // Serial.println(x);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("reconected loop");
+
+  }
+  Serial.println(WiFi.localIP());
+
+  digitalWrite(led,LOW);
+  delay(100);
+  digitalWrite(led,HIGH);
+  delay(100);
+
+
+
 }
---
+++ 180521-210241-nodemcuv2/src/time_day.ino
@@ -0,0 +1,19 @@
+
+#include <time.h>
+String Day() {
+  int timezone = 7 * 3600;                    //Time zone Thailand
+  int dst = 0;                                // Date Swing Time
+  configTime(timezone, dst, "pool.ntp.org", "time.nist.gov");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(100);
+  }
+  time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
+  ctime(&now);
+  char *a[] = {ctime(&now)};
+  String i = String(i + *a);
+  num_day = p_tm->tm_mday;
+
+  return i;
+}
